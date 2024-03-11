65886e77b1e0fb92281be97b1d5de41b7f04e0df
N1co c0des <78597469+n-c0de-r@users.noreply.github.com>
2024-03-11 09:41:22 +0100

    Make WebGL and Win work

--- Assets/Scripts/Intro.cs
+++ Assets/Scripts/Intro.cs
@@ -12,12 +12,30 @@ public class Intro : MonoBehaviour
     [SerializeField]
     private FadeAnimator fadeAnimator;
 
+    [SerializeField]
+    private string videoFileName;
+
+    private void Start()
+    {
+        PlayVideo();
+    }
+
     void Update()
     {
         // Fade out, once the video has played back
         if (player.frame > 0 && !player.isPlaying)
         {
             fadeAnimator.FadeOut();
+        }
+    }
+
+    private void PlayVideo()
+    {
+        if (player)
+        {
+            string videoPath = System.IO.Path.Combine(Application.streamingAssetsPath, videoFileName);
+            player.url = videoPath;
+            player.Play();
         }
     }
 }
